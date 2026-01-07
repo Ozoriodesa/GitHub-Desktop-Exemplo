0a9aca591efa90d2be569d7017ccc53b576c6a72
Andresa Ozorio <emanueligor1774@gmail.com>
2026-01-07 10:15:29 -0300

    Hello World

--- GitHub_Desktop_Exemplo/src/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
+++ GitHub_Desktop_Exemplo/src/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package github_desktop_exemplo;
 
-/**
- *
- * @author JUSSARA
- */
+
 public class GitHub_Desktop_Exemplo {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello, World!");
     }
     
 }
